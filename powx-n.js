f8170da7821f9a57393b4bffe10c96b307c5bd23
kagithapk <kagithapk@gmail.com>
2022-06-10 00:59:52 +0530

    powx-n.js

---
+++ powx-n.js
@@ -0,0 +1,45 @@
+/**
+ * https://leetcode.com/problems/powx-n/
+ * 
+ * Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+Example 1:
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+
+Example 2:
+Input: x = 2.10000, n = 3
+Output: 9.26100
+
+Example 3:
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
+Constraints:
+-100.0 < x < 100.0
+-231 <= n <= 231-1
+-104 <= xn <= 104
+ */
+
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function (x, n) {
+	let result;
+	if (n === 0) return 1;
+	let isNegative = n < 0;
+	if (isNegative) {
+		n = n * -1;
+	}
+
+	if (n % 2 === 0) {
+		result = myPow(x * x, n / 2);
+	} else {
+		result = x * myPow(x * x, (n - 1) / 2);
+	}
+
+	return isNegative ? 1 / result : result;
+};
